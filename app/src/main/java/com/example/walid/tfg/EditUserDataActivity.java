a533a60329bd2b44084bd62ae1429dd46ebcab2f
occ10 <occ10@alu.ua.es>
2018-04-13 17:23:52 +0200

    creacion de la vista edit perfil que contiene los tres fragmentos editUserDataActivity, insertUserCarActivity, editUserFotoActivity

---
+++ app/src/main/java/com/example/walid/tfg/EditUserDataActivity.java
@@ -0,0 +1,13 @@
+package com.example.walid.tfg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditUserDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user_data);
+    }
+}
