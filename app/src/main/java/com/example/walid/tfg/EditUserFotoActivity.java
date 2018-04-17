45ec57295ee8ccf62d11d6565e7c214519520f56
occ10 <occ10@alu.ua.es>
2018-04-17 15:03:31 +0200

    incluir los widgets en la vista editUserFoto

--- app/src/main/java/com/example/walid/tfg/EditUserFotoActivity.java
+++ app/src/main/java/com/example/walid/tfg/EditUserFotoActivity.java
@@ -1,25 +1,93 @@
 package com.example.walid.tfg;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.provider.SyncStateContract;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import net.gotev.uploadservice.MultipartUploadRequest;
+import net.gotev.uploadservice.UploadNotificationConfig;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import static android.app.Activity.RESULT_OK;
 
 public class EditUserFotoActivity extends Fragment {
 
+    //Image request code
+    private int PICK_IMAGE_REQUEST = 1;
+
+    //storage permission code
+    private static final int STORAGE_PERMISSION_CODE = 123;
+
+    //Bitmap to get image from gallery
+    private Bitmap bitmap;
+
+    //Uri to store the image uri
+    private Uri filePath;
+    //Declaring views
+    private Button buttonChoose;
+    private Button buttonUpload;
+    private ImageView imageView;
+    private EditText editText;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+        //Requesting storage permission
+        requestStoragePermission();
     }
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_edit_user_foto, container, false);
+        //Initializing views
+        buttonChoose = (Button) view.findViewById(R.id.selectUserButton);
+        buttonUpload = (Button) view.findViewById(R.id.uploadUserButton);
+        imageView = (ImageView) view.findViewById(R.id.editUserImage);
+
+        //Setting clicklistener
+       // buttonChoose.setOnClickListener((View.OnClickListener) getActivity());
+        //buttonUpload.setOnClickListener((View.OnClickListener) getActivity());
+
         return view;
     }
+
+    //Requesting permission
+    private void requestStoragePermission() {
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+            return;
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            //If the user has denied the permission previously your code will come to this block
+            //Here you can explain why you need this permission
+            //Explain here why you need this permission
+        }
+        //And finally ask for the permission
+        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+    }
+
+
 }
